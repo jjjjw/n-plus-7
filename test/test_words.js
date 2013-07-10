6c75e4abd6e6050afae978073bc40dc8e8568b9c
JJ Weber <jjweber@gmail.com>
2013-07-09 23:38:23 -0700

    Initial commit

---
+++ test/test_words.js
@@ -0,0 +1,17 @@
+var findSubs = require('../lib/words').findSubs;
+var wordList = require('../lib/words').wordList;
+var assert = require('assert');
+
+var sampleText = ["The", "sky"];
+var sampleTargets = [{"sky": 1}];
+
+describe('findSubs', function() {
+  it('should replace a word with the 7th word after it in the word list', function(done) {
+    findSubs(sampleText, sampleTargets, wordList, cb);
+
+    function cb (res) {
+      assert.equal(res[1], "silicone")  // or some such
+      done()
+    }
+  })
+})
