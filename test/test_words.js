0b0f1960007706da5090cf77933e4b10693f2f98
JJ Weber <jjweber@gmail.com>
2013-07-16 21:52:45 -0700

    Finish the word substitution function

--- test/test_words.js
+++ test/test_words.js
@@ -3,14 +3,14 @@ var wordList = require('../lib/words').wordList;
 var assert = require('assert');
 
 var sampleText = ["The", "sky"];
-var sampleTargets = [{"sky": 1}];
+var sampleTargets = {"sky": 1};
 
 describe('findSubs', function() {
   it('should replace a word with the 7th word after it in the word list', function(done) {
     findSubs(sampleText, sampleTargets, wordList, cb);
 
-    function cb (res) {
-      assert.equal(res[1], "silicone")  // or some such
+    function cb (err, res) {
+      assert.equal(res[1], "skylark")
       done()
     }
   })
