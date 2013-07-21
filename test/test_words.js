9d31e1f127cabce14a71a4ac8bff9153e54e912b
JJ Weber <jjweber@gmail.com>
2013-07-21 13:06:26 -0700

    Support substituting words toward the end of the alphabet.

--- test/test_words.js
+++ test/test_words.js
@@ -1,16 +1,26 @@
-var findSubs = require('../lib/words').findSubs;
-var wordList = require('../lib/words').wordList;
-var assert = require('assert');
-
-var sampleText = ["The", "sky"];
-var sampleTargets = {"sky": 1};
+var findSubs = require('../lib/words').findSubs,
+  wordList = require('../lib/words').wordList,
+  assert = require('assert'),
+  sampleText = ["The", "sky"],
+  sampleTextZ = ["The", "Zyzzogeton"], // Depends upon the dictionary used.
+  sampleTargets = {"sky": 1};
+  sampleTargetsZ = {"Zyzzogeton": 1};
 
 describe('findSubs', function() {
   it('should replace a word with the 7th word after it in the word list', function(done) {
-    findSubs(sampleText, sampleTargets, wordList, cb);
+    findSubs(cb, sampleText, sampleTargets, wordList);
 
     function cb (err, res) {
       assert.equal(res[1], "skylark")
+      done()
+    }
+  })
+
+  it('should circle the alphabet in the event of replacing a word at the end of the word list', function(done) {
+    findSubs(cb, sampleTextZ, sampleTargetsZ, wordList);
+
+    function cb (err, res) {
+      assert.equal(res[1], "Aani")
       done()
     }
   })
